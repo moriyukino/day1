f7a45d1b86b8c84194775db1c52af57542dfced8
moriyukino <jieguiyesen0911@gmail.com>
2025-06-07 14:57:31 +0900

    first commit

---
+++ task.py
@@ -0,0 +1,10 @@
+# print("https://appbrewery.github.io/python-day1-demo/")
+print("Well come to the Band Name generator")
+
+city=input("What's the name of the city you grew up in?\n")
+
+print(city)
+
+pet=input("What's your pet's name?\n")
+
+print("Your band name could be : " + "city" + "pet")
